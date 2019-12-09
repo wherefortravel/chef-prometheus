c2a936d556f1313d075abdcdea89a6e190344c96
Larry Rosenman <ler@lerctr.org>
2019-12-09 13:40:10 -0600

    revert ark change

--- recipes/alertmanager_binary.rb
+++ recipes/alertmanager_binary.rb
@@ -23,8 +23,7 @@ include_recipe 'ark::default'
   package pkg
 end
 
-#dir_name = ::File.basename(node['prometheus']['dir'])
-dir_name = 'alertmanager'
+dir_name = ::File.basename(node['prometheus']['dir'])
 dir_path = ::File.dirname(node['prometheus']['dir'])
 
 ark dir_name do
