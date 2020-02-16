1c4ce6c3894e6d40d1042dcd64aef63d7b49e4e0
Larry Rosenman <ler@lerctr.org>
2020-02-15 18:04:03 -0600

    remove hack

--- resources/job.rb
+++ resources/job.rb
@@ -3,8 +3,7 @@ property :scrape_timeout,      String
 property :labels,              Hash
 property :target,              [Array, String]
 property :metrics_path,        String, default: '/metrics'
-property :config_file,         String, default: lazy { node['prometheus']['flags']['config.file'] }
-config_file = "/opt/prometheus/prometheus.yml"
+property :config_file,         String, default: lazy { node['prometheus']['v2_cli_opts']['config.file'] }
 
 default_action :create
 
